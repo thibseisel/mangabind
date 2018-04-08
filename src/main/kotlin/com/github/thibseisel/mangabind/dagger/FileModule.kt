79de08b53f28bfa7bbb6bb22f7f05aaedf2bc0fd
Thibault Seisel <seisel.thibault@gmail.com>
2018-04-08 22:53:14 +0200

    Externalized source catalog with LocalJsonFileLoader

--- src/main/kotlin/com/github/thibseisel/mangabind/dagger/FileModule.kt
+++ src/main/kotlin/com/github/thibseisel/mangabind/dagger/FileModule.kt
@@ -1,6 +1,7 @@
 package com.github.thibseisel.mangabind.dagger
 
 import com.github.thibseisel.mangabind.source.JsonResourceLoader
+import com.github.thibseisel.mangabind.source.LocalJsonFileLoader
 import com.github.thibseisel.mangabind.source.SourceLoader
 import dagger.Binds
 import dagger.Module
@@ -8,5 +9,5 @@ import dagger.Module
 @Module(includes = [FilenameProviderModule::class])
 abstract class FileModule {
 
-    @Binds abstract fun bindsSourceLoader(impl: JsonResourceLoader): SourceLoader
+    @Binds abstract fun bindsSourceLoader(impl: LocalJsonFileLoader): SourceLoader
 }
